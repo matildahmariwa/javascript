ff5ceca4c03f5c4ba3a261718f5040437d35000c
matildahmariwa <matildamariwa3@gmail.com>
2019-12-24 10:55:15 +0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,39 @@
+let
+
+    EL_name = document.getElementById("name"),
+    EL_subject = document.getElementById("subject"),
+    EL_time = document.getElementById("time"),
+    EL_day = document.getElementById("day"),
+    EL_table = document.getElementById("table"), // Our empty table in HTML
+    arr = [];                        // One simple array to store object literals
+
+
+function addTo() {
+
+    // Create an Object literal with out event dara
+    let obj = {
+        name: EL_name.value,
+        subject: EL_subject.value,
+        time: EL_time.value,
+        day: EL_day.value
+
+    };
+
+    // Create Row
+    let row = `<tr>
+               <td>${obj.name}</td>
+               <td>${obj.subject}</td>
+                <td>${obj.time}</td>
+                 <td>${obj.day}</td>
+             </tr>`;
+
+    // Clear inputs
+    EL_name.value = EL_day.value = "";
+
+    // Store into Array
+    arr.push(obj);
+
+    // Insert row into table
+    EL_table.insertAdjacentHTML("beforeend", row);
+
+}
