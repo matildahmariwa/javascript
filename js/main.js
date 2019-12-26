2349dfe36552f6b533f5ce05fe807547d34e85b6
matildahmariwa <matildamariwa3@gmail.com>
2019-12-27 01:54:34 +0300

    added table display

--- js/main.js
+++ js/main.js
@@ -6,29 +6,39 @@ class Schedule {
         this.subject = subject;
         this.time = time;
         this.day = day;
+
     }
 }
 
 
 
-function create(){
+function create() {
 
     let
         name = document.getElementById("name").value,
         subject = document.getElementById("subject").value,
         time = document.getElementById("time").value,
-        day = document.getElementById("day").value
+        day = document.getElementById("day").value,
+        table = document.getElementById("table")
 
 
-    mySchedules.push(new Schedule(name,subject,time,day))
+    mySchedules.push(new Schedule(name, subject, time, day))
 
-}
 
 //create object
-let arr = new Schedule();
+    let obj = new Schedule(name,subject,time,day);
 
+// Create Row
+    let row = `<tr>
+               <td>${obj.name}</td>
+               <td>${obj.subject}</td>
+               <td>${obj.time}</td>
+               <td>${obj.day}</td>
+             </tr>`;
 
+    table.insertAdjacentHTML("beforeend", row);
 
+}
 
 
 
